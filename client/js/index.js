a9c09df8240e847bec885c691974d9b8ba29e36b
LAPOINTE Michel <NQKR0041@ad-subs.w2k.francetelecom.fr>
2017-02-08 09:28:50 +0100

    test server

--- client/js/index.js
+++ client/js/index.js
@@ -73,7 +73,7 @@ function showPopup(e) {
 
 function init() {
 	console.log ("toto a velo");
-	url = apiPath + "getAll";
+	url = apiPath + "mostImportant";
     $.ajax(url, {
         dataType: "json",
         success: function(data) {
